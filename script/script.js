f82a92653ffc0a5f8c9b9cd7901d64fb6c6090a0
Koglmi <40146720180269@litec.ac.at>
2020-08-28 13:34:40 +0200

    can create countdowns

--- script/script.js
+++ script/script.js
@@ -1,33 +1,77 @@
 window.addEventListener("load", () => {
-  const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
+  startCountdown.addEventListener("click", () => {
+    document.getElementById("countdown").style.display = "flex";
+    document.getElementById("setCountdown").style.display = "none";
 
-  let countDown = new Date("Aug 27, 2020 15:00:00").getTime(),
-    x = setInterval(function () {
-      let now = new Date().getTime();
-      let distance = countDown - now;
+    const uebText = document.querySelector('#uebText').value;
+    const colorUeb = document.getElementById('uebColor').value;
 
-      (document.getElementById("days").innerText = Math.floor(distance / day)),
-        (document.getElementById("hours").innerText = Math.floor(
-          (distance % day) / hour
+    const countdownTime = document.querySelector('input[type="datetime-local"]');
+    const countdownColor = document.getElementById('countdownColor').value;
+
+    const afterText = document.querySelector('#afterText').value;
+    const afterColor = document.getElementById('afterColor').value;
+    
+    const backgroundWhile = document.getElementById('colorWhileCountdown').value;
+
+    const backgroundAfter = document.getElementById('colorAfterCountdown').value;
+
+
+    if (countdownTime.value != null) {
+      countDown(countdownTime, countdownColor, uebText, colorUeb, afterText, afterColor, backgroundWhile, backgroundAfter);
+      
+    }
+  });
+
+  function countDown(countdownTime, countdownColor, uebText, colorUeb, afterText, afterColor, backgroundWhile, backgroundAfter) {
+    const second = 1000,
+      minute = second * 60,
+      hour = minute * 60,
+      day = hour * 24;
+
+      let ueb = document.getElementById('uebCountdown')
+      ueb.innerText = uebText; 
+      ueb.style.color = colorUeb;
+      document.body.style.backgroundColor = backgroundWhile;
+      listUL.style.color = countdownColor;
+
+
+    // console.log(countdownTime.value);
+
+    let countDown = new Date(countdownTime.value).getTime(),
+      x = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDown - now;
+
+        (document.getElementById("days").innerText = Math.floor(
+          distance / day
         )),
-        (document.getElementById("minutes").innerText = Math.floor(
-          (distance % hour) / minute
-        )),
-        (document.getElementById("seconds").innerText = Math.floor(
-          (distance % minute) / second
-        ));
+          (document.getElementById("hours").innerText = Math.floor(
+            (distance % day) / hour
+          )),
+          (document.getElementById("minutes").innerText = Math.floor(
+            (distance % hour) / minute
+          )),
+          (document.getElementById("seconds").innerText = Math.floor(
+            (distance % minute) / second
+          ));
 
-      if (distance < 0) {
-        document.getElementById("days").innerText = '0';
-        document.getElementById("hours").innerText = '0';
-        document.getElementById("minutes").innerText = '0';
-        document.getElementById("seconds").innerText = '0';
 
-        clearInterval(x);
-        document.getElementById("afterAll").innerText = 'Juhuuuu es ist Pause!!!';
-      }
-    }, second);
+        if (distance < 0) {
+          document.getElementById("days").innerText = "You";
+          document.getElementById("hours").innerText = "Ma";
+          document.getElementById("minutes").innerText = "de";
+          document.getElementById("seconds").innerText = "it";
+
+          let after = document.getElementById('afterCountdown');
+          after.innerText = afterText;
+          after.style.color = afterColor;
+          
+          document.body.style.backgroundColor = backgroundAfter;
+
+          clearInterval(x);
+          // document.getElementById("afterCountdown").innerText = ;
+        }
+      }, second);
+  }
 });
